3d833459dc7d8c7128acaa12a73b720acb0d88c3
TiagoMA90 <Tiago_MA90@hotmail.com>
2025-04-28 14:30:30 +0000

    update var in env

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+            'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
